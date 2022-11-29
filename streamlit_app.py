7ba840aaf6399273823803f6cd436f344404247c
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-11-29 16:29:12 -0500

    Update streamlit_app.py
    my fruits listed

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ streamlit.header('🍽️Breakfast Menu')
 streamlit.text('🍌Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket🍊 Smoothie')
 streamlit.text('🍇🍉Hard-Boiled Free-Range Egg')
+import pandas 
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
