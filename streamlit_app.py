eb0d1826e87eb931e918f0b91a5af8656d69ff72
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-11-29 16:09:43 -0500

    Update streamlit_app.py
    emotions in family

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 streamlit.title("My family is my world")
-streamlit.title("good vibes of my family is peace")
+streamlit.header("good vibes of my family is peace")
+streamlit.tetx("Happy,love,playing,fighting,interacting")
