999583d21d971fb3c8ddf353fcb667163c7b3c8d
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-11-29 16:43:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ streamlit.text('Kale, Spinach & Rocket🍊 Smoothie')
 streamlit.text('🍇🍉Hard-Boiled Free-Range Egg')
 import pandas 
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# lets put a pick list 
-streamlit.multiselect("Pick fruits:", list(my_fruits_list.index))
 #changing number to fruits name
 my_fruits_list = my_fruits_list.set_index('Fruit')
+# lets put a pick list 
+streamlit.multiselect("Pick fruits:", list(my_fruits_list.index))
+
+
 
 streamlit.dataframe(my_fruits_list)
