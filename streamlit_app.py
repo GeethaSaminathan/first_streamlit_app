258f1d98a9ec85021f7c166d200249faf28bf335
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-11-29 16:36:19 -0500

    Update streamlit_app.py
    added pick list options

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.text('Kale, Spinach & Rocket🍊 Smoothie')
 streamlit.text('🍇🍉Hard-Boiled Free-Range Egg')
 import pandas 
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# lets put a pick list 
+streamlit.multiselect("Pick fruits:", list(my_fruits_list(index))
 streamlit.dataframe(my_fruits_list)
