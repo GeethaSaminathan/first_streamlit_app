cfa8bacecc1af3820d65a22b2406ad90431329c8
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-11-29 16:21:59 -0500

    Update streamlit_app.py
    added emojis using right click

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(🍽️'Breakfast Menu')
+streamlit.text('🍌Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket🍊 Smoothie')
+streamlit.text('🍇🍉Hard-Boiled Free-Range Egg')
