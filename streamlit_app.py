a1b39d495a304a3326a684b205aa1563f0082460
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-12-07 15:20:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ def get_fruit_load_list():
 if streamlit.button('Get FRUIT LOAD LIST'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
   
 #adding second text to streamlit
