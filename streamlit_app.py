0f53d765219b0f0274373ff49f4313681ecea3da
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-12-07 09:43:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.c
 #changing number to fruits name
 my_fruits_list = my_fruits_list.set_index('Fruit')
 # lets put a pick list 
-fruits_selected = streamlit.multiselect("Pick fruits:", list(my_fruits_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick fruits:", list(my_fruits_list.index))
 fruits_to_show = my_fruits_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.write('Thanks for choosing ',fruits_selected)
