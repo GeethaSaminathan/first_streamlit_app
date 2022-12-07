d9a9cc219a2ca698e4387e4f02557b4cc6941a96
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-12-07 10:41:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,22 @@
 import streamlit
 import pandas 
-
-
-
+import snowflake.connector
+import requests
+from urllib.error import URLError
+# import pandas
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #changing number to fruits name
 my_fruits_list = my_fruits_list.set_index('Fruit')
-# lets put a pick list 
-#,['Avocado','Strawberries']
+#lets select pick list
 fruits_selected = streamlit.multiselect("Pick fruits:", list(my_fruits_list.index))
 fruits_to_show = my_fruits_list.loc[fruits_selected]
+#display the table on the page
 streamlit.dataframe(fruits_to_show)
-#streamlit.write('Thanks for adding ' ,fruits_selected)
 #NEW SECTION TO DISPLAY FRUITYVICE API RESPONSE
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 #importind request
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
@@ -27,8 +26,6 @@ streamlit.dataframe(fruityvice_normalized)
 # stopping tempor
 streamlit.stop()
 #importing snowflakes connectors
-import snowflake.connector
-
 # query for trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -39,6 +36,6 @@ streamlit.header("The FRUIT LIST contains:")
 streamlit.dataframe(my_data_rows)
 #
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
+#adding second text to streamlit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
