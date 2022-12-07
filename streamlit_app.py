6273971f64346a1147a1836f91bd327c1747cbb9
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-12-07 07:39:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import matplotlib.pyplot
 import pandas 
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #changing number to fruits name
