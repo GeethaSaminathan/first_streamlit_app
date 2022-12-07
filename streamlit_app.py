51ef4a974c8bac9e982034374ea46e2457cd3424
Geetha <85403656+GeethaSaminathan@users.noreply.github.com>
2022-12-07 11:07:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,27 @@ fruits_selected = streamlit.multiselect("Pick fruits:", list(my_fruits_list.inde
 fruits_to_show = my_fruits_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+#create function
+def get_fruittyvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #NEW SECTION TO DISPLAY FRUITYVICE API RESPONSE
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-#importind request
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+       streamlit.error("Please select fruit to get info.")
+  else:
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+ except URLError as e:
+      streamlit.error()
+
+
+
 # stopping tempor
 streamlit.stop()
 #importing snowflakes connectors
